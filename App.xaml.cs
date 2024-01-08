e01f560ccd34647181c572c4594bbddae6424b96
KaayaTech <jayanthd26@gmail.com>
2024-01-08 23:42:55 +0530

    UI Project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mingle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
