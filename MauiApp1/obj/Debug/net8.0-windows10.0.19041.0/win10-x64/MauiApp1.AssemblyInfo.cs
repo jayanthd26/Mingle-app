106dca58df0d05ecc47ff5106952b63e9114e874
KaayaTech <jayanthd26@gmail.com>
2024-03-22 00:13:47 +0530

    Pages WIP

--- MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fb8887c79855575e4ac15a9e0c85ca8e6bd15293")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
